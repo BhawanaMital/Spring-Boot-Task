8b6b6746ca61e1b48d6ea34a4c372b2a3dc29f22
BhawanaMital <you@example.com>
2019-07-24 23:59:08 +0530

    Swagger API

--- src/main/java/com/stackroute/Muzixapp/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/Muzixapp/config/SwaggerConfig.java
@@ -1,12 +1,16 @@
 package com.stackroute.Muzixapp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket productApi() {
@@ -16,3 +20,4 @@ public class SwaggerConfig {
                 .build();
     }
 }
+
